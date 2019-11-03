9e4df60d09ac685c5050635bfe3d26a62233b4c2
administrator ProTrack <administrator@protrack.co.il>
2019-11-03 18:44:17 +0200

    add py function for computation

---
+++ ModularCalc.py
@@ -0,0 +1,14 @@
+import numpy as np
+from numpy import linalg as LA
+
+
+def GetEigen_WithWeights (Prov_mat, Cons_mat, Prov_Weight, Cons_Weight):
+    dim = Prov_mat.size
+    union_mat = Prov_mat*Prov_Weight + Cons_mat*Cons_Weight
+    row_sum = np.sum(union_mat,axis = 1, dtype=int)
+    col_sum = np.sum(union_mat,axis = 0, dtype=int)
+    deg_mat = np.diag(np.concatenate((col_sum,row_sum)))
+    adj_mat = np.block([[np.zeros((dim,dim)),union_mat.T], [union_mat, np.zeros((dim,dim))]])
+    laplacian = np.matrix(deg_mat-adj_mat, dtype=int)
+    v, w = LA.eig(laplacian)
+    return v,w
