1854a83729c2b5efbc792bdc11e7375c73796a30
Ella Gitul <harella1@gmail.com>
2019-12-09 10:07:33 +0200

    functionals first

--- ModularCalc.py
+++ ModularCalc.py
@@ -7,7 +7,8 @@ def GetEigen_WithWeights (Prov_mat, Cons_mat, Prov_Weight, Cons_Weight):
     union_mat = Prov_mat*Prov_Weight + Cons_mat*Cons_Weight
     row_sum = np.sum(union_mat,axis = 1, dtype=int)
     col_sum = np.sum(union_mat,axis = 0, dtype=int)
-    deg_mat = np.diag(np.concatenate((col_sum,row_sum)))
+    deg_mat = np.diag(np.concatenate((row_sum,col_sum)))
+    adj_mat = np.block([[np.zeros((dim,dim)), union_mat], [union_mat.T,np.zeros((dim,dim))]])
     adj_mat = np.block([[np.zeros((dim,dim)),union_mat.T], [union_mat, np.zeros((dim,dim))]])
     laplacian = np.matrix(deg_mat-adj_mat, dtype=int)
     v, w = LA.eig(laplacian)
